d13bdc8a58d95e0d4266ce03fa1c42bf2c5804a7
Seaborn Lee <seabornlee.cn@gmail.com>
2022-11-18 22:48:07 +0800

    Update config

--- build.py
+++ build.py
@@ -5,7 +5,6 @@ import urllib.parse
 import datetime
 
 def fetch_ci_time(filePath):
-    print("https://api.github.com/repos/tw93/weekly/commits?path=" + filePath + "&page=1&per_page=1")
     entries = httpx.get("https://api.github.com/repos/seabornlee/cards/commits?path=" + filePath + "&page=1&per_page=1")
     ciTime= entries.json()[0]["commit"]["committer"]["date"].split("T")[0]
     return ciTime
@@ -13,7 +12,7 @@ def fetch_ci_time(filePath):
 
 if __name__ == "__main__":
   readmefile=open('README.md','w')
-  readmefile.write("# 周记\n\n> 记录 Seaborn 的生活，欢迎订阅，Fork 自用可见 [开发文档](https://github.com/tw93/weekly/blob/main/Deploy.md)，期待你玩得开心~\n\n")
+  readmefile.write("# Life\n\n> 记录 Seaborn 的生活，欢迎订阅，Fork 自用可见 [开发文档](https://github.com/tw93/weekly/blob/main/Deploy.md)，期待你玩得开心~\n\n")
   recentfile=open('RECENT.md','w')
 
   for root, dirs, filenames in os.walk('./src/pages/posts'):
@@ -23,18 +22,11 @@ if __name__ == "__main__":
   for index, name in enumerate(filenames):
       if name.endswith('.md'):
         filepath = urllib.parse.quote(name)
-        oldTitle = name.split('.md')[0]
-        url   = 'https://weekly.seabornlee.cn/posts/' + oldTitle
-        title = '第 ' + oldTitle.split('-')[0] + ' 期 - ' + oldTitle.split('-')[1];
+        title = name.split('.md')[0]
+        url   = 'https://life.seabornlee.cn/posts/' + oldTitle
         readmeMd= '* [{}]({})\n'.format(title, url)
-        dateList = ["2022-10-10","2022-09-26","2022-09-12","2022-09-05","2022-08-29"]
-        num = int(oldTitle.split('-')[0])
         if index < 5 :
-          if num < 100 :
-            modified = dateList[99-num]
-          else :
-            modified = fetch_ci_time('/src/pages/posts/' + filepath)
-
+          modified = fetch_ci_time('/src/pages/posts/' + filepath)
           recentMd= '* [{}]({}) - {}\n'.format(title, url, modified)
           recentfile.write(recentMd)
         readmefile.write(readmeMd)
