1fbf47810c7085fe2df6d77f022a8d3ca672f1af
Seaborn Lee <seabornlee.cn@gmail.com>
2023-11-05 15:22:34 +0800

    Sort posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,30 @@
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import dayjs from 'dayjs';
-import fs from 'fs';
-import fse from 'fs-extra';
-import { defineConfig } from 'astro/config';
-import { parse } from 'node-html-parser';
-import { SITE } from './src/config';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import dayjs from "dayjs";
+import fs from "fs";
+import fse from "fs-extra";
+import { defineConfig } from "astro/config";
+import { parse } from "node-html-parser";
+import { SITE } from "./src/config";
 
 function defaultLayoutPlugin() {
   return function (tree, file) {
     const filePath = file.history[0];
-    file.data.astro.frontmatter.layout = '@layouts/post.astro';
+    file.data.astro.frontmatter.layout = "@layouts/post.astro";
 
     // 头图放到文档中的第一行，会自动帮你处理，也可以用 frontmatter 方式，赋值给 pic 字段
     let hasCover = false;
     if (tree.children[0]?.value) {
-      const imageElement = parse(tree.children[0].value).querySelector('img');
-      file.data.astro.frontmatter.pic = imageElement.getAttribute('src');
+      const imageElement = parse(tree.children[0].value).querySelector("img");
+      file.data.astro.frontmatter.pic = imageElement.getAttribute("src");
       hasCover = true;
     }
 
     // 描述放到文档中头图的下一行，会自动帮你处理，也可以用 frontmatter 方式，赋值给 desc 字段
     const descIndex = hasCover ? 1 : 0;
     if (tree.children[descIndex]?.children[0]?.value) {
-      file.data.astro.frontmatter.desc = tree.children[descIndex].children[0].value;
+      file.data.astro.frontmatter.desc =
+        tree.children[descIndex].children[0].value;
     }
 
     const { date, desc, pic } = file.data.astro.frontmatter;
@@ -40,10 +41,7 @@ function defaultLayoutPlugin() {
 
     //这里也可以直接在 frontmatter，赋值给 date 字段
     if (!date) {
-      const createDate = dayjs(fs.statSync(filePath).atime).format(
-        'YYYY/MM/DD',
-      );
-      file.data.astro.frontmatter.date = createDate;
+      file.data.astro.frontmatter.date = "2010-10-10";
     }
   };
 }
